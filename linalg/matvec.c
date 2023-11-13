53c78de1a76ca73e5ff852e30190c64449d889fa
Johannes Schuster <johannes.schuster@physik.uni-regensburg.de>
2023-11-13 11:32:50 +0100

    Added line and column permutation for matrices

--- linalg/matvec.c
+++ linalg/matvec.c
@@ -11,3 +11,29 @@ vec mat_vec_mult(mat const A, vec const b, int const m, int const n)
         }
     }
 }
+
+void mat_permute_lines(mat a, ivec const p, int const m, int const n)
+{
+    for (int i=0; i<m-1; ++i)
+    {
+        for (int j=0; j<n; ++j)
+        {
+            double buff = a[p[i]*n+j];
+            a[p[i]*n+j] = a[i*n+j];
+            a[i*n+j] = buff;
+        }
+    }
+}
+
+void mat_permute_cols(mat a, ivec const p, int const m, int const n)
+{
+    for (int j=0; j<n-1; ++j)
+    {
+        for (int i=0; i<m; ++i)
+        {
+            double buff = a[i*n+p[j]];
+            a[i*n+p[j]] = a[i*n+j];
+            a[i*n+j] = buff;
+        }
+    }
+}
--- linalg/matvec.h
+++ linalg/matvec.h
@@ -6,6 +6,8 @@
 
 vec mat_vec_mult(mat const A, vec const b, int const m, int const n);
 
-void mat_permute(mat a, ivec const p);
+void mat_permute_lines(mat a, ivec const p, int const m, int const n);
+void mat_permute_cols(mat a, ivec const p, int const m, int const n);
+
 
 #endif // LINALG_MATVEC_H
