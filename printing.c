28924dde5811ad7d41e19d673c3efce70edabaea
Johannes Schuster <johannes.schuster@physik.uni-regensburg.de>
2023-11-13 09:33:04 +0100

    Implement printing functions for vec and mat

---
+++ printing.c
@@ -0,0 +1,19 @@
+#include "printing.h"
+#include "stdio.h"
+
+void vec_print(vec const a, int const n)
+{
+    for (int i=0; i<n; ++i)
+    {
+        printf("%1.2lf ", a[i]);
+    }
+}
+
+void mat_print(mat const a, int const m, int const n)
+{
+    for (int i=0; i<n; ++i)
+    {
+        vec_print(a+i*m, m);
+        printf("\n");
+    }
+}
---
+++ printing.h
@@ -0,0 +1,10 @@
+#ifndef PRINTING_H
+#define PRINTING_H
+
+#include "vector.h"
+#include "matrix.h"
+
+void vec_print(vec const a, int const n);
+void mat_print(mat const a, int const m, int const n);
+
+#endif
