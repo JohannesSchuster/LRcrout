2a363317f563f392879b1135537056749a135926
Johannes Schuster <johannes.schuster@physik.uni-regensburg.de>
2023-11-13 09:23:50 +0100

    Changed names for vector-functions to have vec_ in ftont

--- vector.c
+++ vector.c
@@ -4,73 +4,73 @@
 #include <string.h>
 #include <math.h>
 
-vec alloc(int const n)
+vec vec_alloc(int const n)
 {
     return (vec)malloc(n*sizeof(double));
 }
 
-vec zero(int const n)
+vec vec_zero(int const n)
 {
     return (vec)calloc(n, sizeof(double));
 }
 
-vec unit(int const d, int const n)
+vec vec_unit(int const d, int const n)
 {
-    vec res = zero(n);
+    vec res = vec_zero(n);
     res[d] = 1;
     return res;
 }
 
-vec one(int const n)
+vec vec_one(int const n)
 {
-    vec res = alloc(n);
+    vec res = vec_alloc(n);
     memset(res, (double)1, n*sizeof(double));
     return res;
 }
 
-vec copy(vec const a, int const n)
+vec vec_copy(vec const a, int const n)
 {
-    vec res = alloc(n);
+    vec res = vec_alloc(n);
     memcpy(res, a, n*sizeof(double));
 }
 
-void scaleInp(vec a, double const s, int const n)
+void vec_scaleInp(vec a, double const s, int const n)
 {
     for (int i=0; i<n; ++i) a[i] *= s;
 }
 
-void addInp(vec a, vec const b, int const n)
+void vec_addInp(vec a, vec const b, int const n)
 {
     for (int i=0; i<n; ++i) a[i] += b[i];
 }
 
-void subInp(vec a, vec const b, int const n)
+void vec_subInp(vec a, vec const b, int const n)
 {
     for (int i=0; i<n; ++i) a[i] -= b[i];
 }
 
-vec scale(vec const a, double const s, int const n)
+vec vec_scale(vec const a, double const s, int const n)
 {
-    vec res = copy(a, n);
-    scaleInp(res, s, n);
+    vec res = vec_copy(a, n);
+    vec_scaleInp(res, s, n);
     return res;
 }
 
-vec add(vec const a, vec const b, int const n)
+vec vec_add(vec const a, vec const b, int const n)
 {
-    vec res = copy(a, n);
-    addInp(res, b, n);
+    vec res = vec_copy(a, n);
+    vec_addInp(res, b, n);
     return res;
 }
 
-vec sub(vec const a, vec const b, int const n)
+vec vec_sub(vec const a, vec const b, int const n)
 {
-    vec res = copy(a, n);
-    subInp(res, b, n);
+    vec res = vec_copy(a, n);
+    vec_subInp(res, b, n);
     return res;
 }
 
-double scalarProd(vec const a, vec const b, int const n)
+double vec_scalarProd(vec const a, vec const b, int const n)
 {
     double prod = 0;
     for (int i=0; i<n; ++i) 
@@ -78,23 +78,23 @@ double scalarProd(vec const a, vec const b, int const n)
     return prod;
 }
 
-double sqrNorm (vec const a, int const n)
+double vec_sqrNorm (vec const a, int const n)
 {
-    return scalarProd(a, a, n);
+    return vec_scalarProd(a, a, n);
 }
 
-double norm(vec const a, int const n)
+double vec_norm(vec const a, int const n)
 {
-    return sqrt(sqrNorm(a, n));
+    return sqrt(vec_sqrNorm(a, n));
 }
 
-double sqrDist(vec const a, vec const b, int const n)
+double vec_sqrDist(vec const a, vec const b, int const n)
 {
-    vec d = sub(b, a, n);
-    return sqrNorm(d, n);
+    vec d = vec_sub(b, a, n);
+    return vec_sqrNorm(d, n);
 }
 
-double dist(vec const a, vec const b, int const n)
+double vec_dist(vec const a, vec const b, int const n)
 {
-    return sqrt(sqrDist(a, b, n));
+    return sqrt(vec_sqrDist(a, b, n));
 }
--- vector.h
+++ vector.h
@@ -3,25 +3,25 @@
 
 typedef double* vec;
 
-vec zero(int const n);
-vec unit(int const d, int const n);
-vec one(int const n);
+vec vec_zero(int const n);
+vec vec_unit(int const d, int const n);
+vec vec_one(int const n);
 
-vec copy(vec const a, int const n);
+vec vec_copy(vec const a, int const n);
 
-void scaleInp(vec a, double const s, int const n);
-void addInp(vec a, vec const b, int const n);
-void subInp(vec a, vec const b, int const n);
+void vec_scaleInp(vec a, double const s, int const n);
+void vec_addInp(vec a, vec const b, int const n);
+void vec_subInp(vec a, vec const b, int const n);
 
-vec scale(vec const a, double const s, int const n);
-vec add(vec const a, vec const b, int const n);
-vec sub(vec const a, vec const b, int const n);
+vec vec_scale(vec const a, double const s, int const n);
+vec vec_add(vec const a, vec const b, int const n);
+vec vec_sub(vec const a, vec const b, int const n);
 
-double scalarProd(vec const a, vec const b, int const n);
-double sqrNorm (vec const a, int const n);
-double norm(vec const a, int const n);
+double vec_scalarProd(vec const a, vec const b, int const n);
+double vec_sqrNorm (vec const a, int const n);
+double vec_norm(vec const a, int const n);
 
-double sqrDist(vec const a, vec const b, int const n);
-double dist(vec const a, vec const b, int const n);
+double vec_sqrDist(vec const a, vec const b, int const n);
+double vec_dist(vec const a, vec const b, int const n);
 
 #endif // VECTOR_H
