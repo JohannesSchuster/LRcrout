eed952fccaae70d3524a4c13702ad7a34925194f
Johannes Schuster <johannes.schuster@physik.uni-regensburg.de>
2023-11-13 09:23:18 +0100

    Implement functions for matrix

---
+++ matrix.h
@@ -0,0 +1,21 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef double* mat;
+
+mat mat_alloc(int const m, int const n);
+mat mat_zero(int const m, int const n);
+mat mat_identity(int const m, int const n);
+mat mat_copy(mat const a, int const m, int const n);
+
+void mat_scaleInp(mat a, double const s, int const m, int const n);
+void mat_addInp(mat a, mat const b, int const m, int const n);
+void mat_subInp(mat a, mat const b, int const m, int const n);
+
+mat mat_scale(mat const a, double const s, int const m, int const n);
+mat mat_add(mat const a, mat const b, int const m, int const n);
+mat mat_sub(mat const a, mat const b, int const m, int const n);
+mat mat_mult(mat const a, mat const b, int const l, int const m, int const n);
+
+
+#endif // MATRIX_H
