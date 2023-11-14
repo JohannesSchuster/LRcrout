ec111be59367ce96451db14d2d5fc84d23db848a
Johannes Schuster <johannes.schuster@physik.uni-regensburg.de>
2023-11-14 14:45:30 +0100

    Deleted old stuff

--- blatt4.h
+++
@@ -1,7 +0,0 @@
-#ifndef BLATT_4_h
-#define BLATT_4_h
-
-void LRcrout(double *A, int const n);
-void Mperm(double *A, int const *pi, int const m, int const n);
-
-#endif // BALTT_4_h
--- utils.c
+++
@@ -1,6 +0,0 @@
-#include "utils.h"
-
-int min(int const a, int const b)
-{
-    return (a < b ? a : b);
-}
