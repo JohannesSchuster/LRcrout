51eaf9b41da0f379c477dfeaf5dde73625c15f83
Johannes Schuster <johannes.schuster@physik.uni-regensburg.de>
2023-11-15 17:05:40 +0100

    Added lr decomposition with pivoting

--- linalg/algorithms/lr.c
+++ linalg/algorithms/lr.c
@@ -34,27 +34,152 @@ int lrInp(mat A, int const n, double const eps)
             }
             if (dabs(A[i*n+i]) < eps)
             {
-                printf("LR: Diagonal element %i too small (%lf), aborted!\n", i+1, A[i*n+i]);
-                return;
+                printf("LR: Pivot element %i too small (%lf), aborted!\n", i+1, A[i*n+i]);
+                return 0;
             }
 
             // Evaluating L(k, i)
             A[j*n+i] = (A[i*n+j] - sum) / A[i*n+i];
         }
     }
+    return 1;
 }
 
 //LR decomposition with row permutation 
-// void lr_pivot_colsInp(mat A, ivec p, int const n, double const eps)
-// {
-// 
-// }
+int lr_pivot_colsInp(mat A, ivec pi, int const n, double const eps)
+{
+    //Initialize p
+    for (int i=0; i<n; ++i) 
+    {
+        pi[i] = i;
+    }
+
+    //Store the number of transpositions in pi[n]
+    pi[n] = 0;
+
+    for (int k=0; k<n-1; ++k)
+    {
+        double max = 0;
+        int p = 0;
+        for (int j=k; j<n; ++j)
+        {
+            if (dabs(A[j*n+k]) > max)
+            {
+                max = A[j*n+k];
+                p = j;
+            }
+        }
+
+        //Only swap when neccessary
+        if (p!=k)
+        {
+            //swap pi[p] and pi[k]
+            {
+                int buff = pi[k];
+                pi[k] = pi[p];
+                pi[p] = buff;
+            }
+
+            //swap lines A[p,1:n] and A[k,1:n]
+            for (int j=0; j<n; ++j)
+            {
+                double buff = A[k*n+j];
+                A[k*n+j] = A[p*n+j];
+                A[p*n+j] = buff;
+            }
+
+            pi[n]++;
+        }
+
+        //Check the pivot element, 
+        if (dabs(A[k*n+k]) < eps)
+        {
+            //If too small A is singular
+            printf("LR: Pivot element %i too small (%lf), aborted!\n", k+1, A[k*n+k]);
+            return 0;
+        }
+
+        for (int i=k+1; i<n; ++i)
+        {
+            A[i*n+k] /= A[k*n+k];
+            double sum = 0;
+            for (int j=k+1; j<n; ++j)
+            {
+                A[i*n+j] -= A[i*n+k] * A[k*n+j];
+            }
+        }
+    }
+
+    return 1;
+}
 
 //LR decomposition with line permutation
-// void lr_pivot_linesInp(mat A, ivec p, int const n, double const eps)
-// {
-// 
-// }
+int lr_pivot_linesInp(mat A, ivec pi, int const n, double const eps)
+{
+    //Initialize p
+    for (int i=0; i<n; ++i) 
+    {
+        pi[i] = i;
+    }
+    
+    //Store the number of transpositions in pi[n]
+    pi[n] = 0;
+
+    for (int k=0; k<n-1; ++k)
+    {
+        double max = 0;
+        int p = 0;
+        for (int j=k; j<n; ++j)
+        {
+            if (dabs(A[k*n+j]) > max)
+            {
+                max = A[k*n+j];
+                p = j;
+            }
+        }
+
+        //Only swap when neccessary
+        if (p!=k)
+        {
+            //swap pi[p] and pi[k]
+            {
+                int buff = pi[k];
+                pi[k] = pi[p];
+                pi[p] = buff;
+            }
+
+            //swap cols A[1:n,p] and A[1:n,p]
+            for (int j=0; j<n; ++j)
+            {
+                double buff = A[j*n+k];
+                A[j*n+k] = A[j*n+p];
+                A[j*n+p] = buff;
+            }
+
+            pi[n]++;
+        }
+
+        //Check the pivot element, 
+        if (dabs(A[k*n+k]) < eps)
+        {
+            //If too small A is singular
+            printf("LR: Pivot element %i too small (%lf), aborted!\n", k+1, A[k*n+k]);
+            return 0;
+        }
+
+        for (int i=k+1; i<n; ++i)
+        {
+            A[k*n+i] /= A[k*n+k];
+            double sum = 0;
+            for (int j=k+1; j<n; ++j)
+            {
+                A[i*n+j] -= A[i*n+k] * A[k*n+j];
+            }
+        }
+    }
+
+    return 1;
+}
 
 //LR decomposition but don't override A
 void **lr(mat const A, int const n, double const eps)
@@ -81,8 +206,33 @@ void **lr(mat const A, int const n, double const eps)
     }
     
     //Create a suitable return type
-    void **ret = (void**)malloc(2*sizeof(void*));
-    ret[0] = (void*)L;
-    ret[1] = (void*)R;
-    return ret;
+    return bind(2, L, R);
+}
+
+void **lr_pivot_lines(mat const A, int const n, double const eps)
+{
+    mat R = mat_copy(A, n, n);
+    ivec p = (ivec)malloc((n+1)*sizeof(int));
+
+    if (lr_pivot_linesInp(R, p, n, eps) == 0)
+    {
+        free(R);
+        free(p);
+        return NULL;
+    }
+
+    mat L = mat_identity(n, n);
+
+    //Split the matrices O(n2)
+    for (int i=0; i<n; ++i)
+    {
+        for (int j=0; j<i; ++j)
+        {
+            L[i*n+j] = R[i*n+j];
+            R[i*n+j] = 0;
+        }
+    }
+
+    return bind(3, L, R, p);
+
 }
